dba6c90b4eaa9dc4d07c980b63743f609441c79b
NaixSenpai <naixsenpai@gmail.com>
2020-07-04 13:21:50 +0430

    initialing

---
+++ js/adviser.js
@@ -0,0 +1,26 @@
+const advisers = [
+    {
+        name: 'میلاد جعفری',
+        pic: 'https://karmento.ir/upload/images/avatar/2020/14/300x300_342675-600_7679139D1BD52E-0A70-4DC0-9692-F3679D94B309.jpg',
+        title: 'مالیاتی'
+    },
+    {
+        name: 'امید یحی پور',
+        pic: 'https://karmento.ir/upload/images/avatar/2020/14/300x300_213778-900_265196300_388468IMG-20190130-WA0006.jpg',
+        title: 'مالیاتی'
+    },
+    {
+        name: 'محمد غیاثی',
+        pic: 'https://karmento.ir/upload/images/profile/2019/19/300_790264IMG-20190512-WA0002~2.jpg',
+        title: 'حسابداری'
+    },
+    {
+        name: 'زهرا مومنی',
+        pic: 'https://karmento.ir/upload/images/avatar/2020/14/300x300_92635-600_855567-20200221_134650.jpg',
+        title: 'دیجیتال مارکتینگ'
+    },
+]
+
+const name = $('#adviser__chosen__name');
+const title = $('#adviser__chosen__title');
+const avatar = $('#adviser__chosen__img');
