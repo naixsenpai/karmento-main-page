1d4ddc627fcd0c9132af95afa2a39de3efca4f50
naixsenpai <naixsenpai@gmail.com>
2020-08-23 03:57:11 -0700

    the end

--- js/adviser.js
+++ js/adviser.js
@@ -1,5 +1,6 @@
 const name = $('#adviser__chosen__name');
 const title = $('#adviser__chosen__title');
+const category = $('#adviser__chosen__category');
 // const avatar = $('#adviser__chosen__img');
 const list = $('#adviser__list');
 const advisers = [];
@@ -8,24 +9,28 @@ list.children().map((__, element) => {
     advisers.push({
         name: element.dataset.name,
         title: element.dataset.title,
-        category: element.dataset.category,
+        category: element.dataset.category.split(','),
         pic: element.currentSrc,
     })
 });
 
-// list.niceScroll();
-// $('body').niceScroll();
 
 function change(index) {
     name.text(advisers[index].name);
     title.text(advisers[index].title);
-    // avatar.attr('src', advisers[index].pic);
+    advisers[index].category.forEach(element => {
+        category.append(`<p>${element}</p>`);
+    })
 }
 
 function changeListOrder() {
     const element = list.children()[0];
     list.children()[0].remove();
     list.append(element);
+}
+
+function clearCategory() {
+    category.empty();
 }
 
 function initialData() {
@@ -37,6 +42,7 @@ initialData();
 var current = 1;
 
 const automaticChange = setInterval(function() {
+    clearCategory();
     change(current);
     changeListOrder();
     current = current + 1;
--- js/phone_adviser.js
+++ js/phone_adviser.js
@@ -1,15 +1,23 @@
 const phone_name = $('#phone_adviser_name');
 const phone_title = $('#phone_adviser_title');
 const phone_avatar = $('#phone_adviser_avatar');
+const phone_category = $('#phone_adviser_category');
 
 function phone_change(index) {
     phone_name.text(advisers[index].name);
     phone_title.text(advisers[index].title);
     phone_avatar.attr('src', advisers[index].pic);
+    advisers[index].category.forEach(element => {
+        phone_category.append(`<p>${element}</p>`);
+    })
 }
 
 function phone_initialData() {
     phone_change(0);
+}
+
+function clearPhoneCategory() {
+    phone_category.empty();
 }
 
 phone_initialData();
@@ -17,9 +25,10 @@ phone_initialData();
 var phone_current = 1;
 
 const phone_automaticChange = setInterval(function() {
+    clearPhoneCategory();
     phone_change(phone_current);
     phone_current = phone_current + 1;
     if (phone_current == advisers.length) {
         phone_current = 0;
     }
-}, 3000);
+}, 3500);
